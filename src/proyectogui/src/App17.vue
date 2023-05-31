4fa1476befb72939e7c035e6c60184ebdd9cdbb5
Salma Cruz Sánchez <63065531+SalmaCzSz@users.noreply.github.com>
2023-05-31 16:09:40 -0600

    31/05/2023 | Uso de axios para validar el tipo de dato de las propiedades de un componente.

---
+++ src/proyectogui/src/App17.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <componenteUsuarios v-for="usuario in usuarios"
+                        v-bind:key="usuario.id"
+                        :nombre="1"
+                        :email="usuario.email"
+                        :telefono="usuario.phone">
+    </componenteUsuarios>
+  </div>
+</template>
+
+<script>
+ import axios from 'axios'
+ import componenteUsuarios from "./components/usuarios";
+
+export default {
+  name: 'App',
+  components: {
+    componenteUsuarios
+  },
+  data(){
+    return{
+        usuarios: [],
+    }
+  },
+  methods: {
+    obtenerUsuarios(){
+        axios.get('https://jsonplaceholder.typicode.com/users')
+             .then((respuesta) => this.usuarios = respuesta.data)
+    }
+  },
+  mounted(){
+    this.obtenerUsuarios()
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ src/proyectogui/src/components/usuarios.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>Nombre: {{nombre}}</p>
+        <p>Email: {{email}}</p>
+        <p>Telefono: {{telefono}}</p>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "componenteUsuarios",
+        props: {
+            nombre: {
+                type: String,
+                required: true,
+                default: 'Salery'
+            },
+            email: String,
+            telefono: String,
+        }
+    }
+</script>
+
+<style scoped>
+</style>
