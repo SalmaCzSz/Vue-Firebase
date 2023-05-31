19cc6570af37cf2ebbfd43ce2ff81ae22608f60d
Salma Cruz Sánchez <63065531+SalmaCzSz@users.noreply.github.com>
2023-05-31 16:19:21 -0600

    31/05/2023 | Se retoma el archivo en que se uso la función renderizar para mostrar el uso de CSS Scoped.

---
+++ src/proyectogui/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Componente padre</h1>
+    <hr>
+    <componenteElemento tipo="h1"> Texto de este elemento </componenteElemento>
+    <componenteElemento tipo="h2"> Texto de este elemento </componenteElemento>
+    <componenteElemento tipo="h3"> Texto de este elemento </componenteElemento>
+    <componenteElemento tipo="aside"> Contenido de aside </componenteElemento>
+    <componenteElemento tipo="h1"> Contenido de aside </componenteElemento>
+  </div>
+</template>
+
+<script>
+import componenteElemento from "./components/componenteElemento";
+
+export default {
+  name: 'App',
+  components: {
+    componenteElemento
+  }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: blue;
+    }
+</style>
