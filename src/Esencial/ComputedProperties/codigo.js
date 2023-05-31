5aada933059335c92d9e04072d15236e3e817016
Salma Cruz Sánchez <63065531+SalmaCzSz@users.noreply.github.com>
2023-05-31 15:30:51 -0600

    26/05/2023 | Ejemplo del uso de computed properties en un ToDo List.

---
+++ src/Esencial/ComputedProperties/codigo.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+    el: '#main',
+    data:{
+        tarea: null,
+        tareas: [
+            {titulo: 'Aprender ES6', completado: false },
+            {titulo: 'Aprender VUE', completado: false },
+            {titulo: 'Descansar toda la tarde', completado: false },
+            {titulo: 'Traducir documentos T24', completado: true }
+        ]
+    },
+    methods:{
+        agregarTarea(){
+            this.tareas.unshift({titulo:this.tarea, completado:false})
+        }
+    },
+    computed:{
+        mostrarCompletadas(){
+            return this.tareas.filter(item => item.completado)
+        }
+    }
+})
