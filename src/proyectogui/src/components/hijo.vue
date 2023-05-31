a905dee4c48dd1ef05efcb2616ea4e2f9d261999
Salma Cruz Sánchez <63065531+SalmaCzSz@users.noreply.github.com>
2023-05-31 16:48:03 -0600

    31/05/2023 | Propiedades en los componentes. Complemento de App16.vue y persona.vue

---
+++ src/proyectogui/src/components/hijo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Nombre: {{nombre}}</p>
+        <p>Edad: {{edad}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "componenteHijo",
+        props: [
+            'nombre',
+            'edad'
+        ]
+    }
+</script>
+
+<style scoped>
+</style>
