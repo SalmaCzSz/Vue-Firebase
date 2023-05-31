9a9bc9ca5b231dec562a45acd828041b210b3104
Salma Cruz Sánchez <63065531+SalmaCzSz@users.noreply.github.com>
2023-05-31 16:16:14 -0600

    31/05/2023 | Función render.

---
+++ src/proyectogui/src/App20.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <componenteElemento tipo="h1"> Texto de este elemento </componenteElemento>
+    <componenteElemento tipo="h2"> Texto de este elemento </componenteElemento>
+    <componenteElemento tipo="h3"> Texto de este elemento </componenteElemento>
+    <componenteElemento tipo="aside"> Contenido de aside </componenteElemento>
+    <componenteElemento tipo="h1"> Contenido de aside </componenteElemento>
+  </div>
+</template>
+
+<script>
+import componenteElemento from "./components/componenteElemento";
+
+export default {
+  name: 'App',
+  components: {
+    componenteElemento
+  }
+}
+</script>
+
+<style>
+</style>
