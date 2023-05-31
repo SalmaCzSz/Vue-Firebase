523b965bbfb90574da353fabdd6538bf3f7508d3
Salma Cruz Sánchez <63065531+SalmaCzSz@users.noreply.github.com>
2023-05-31 16:11:24 -0600

    31/05/2023 | Uso de slots y nameSlots.

---
+++ src/proyectogui/src/App18.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <componenteSlot>
+        Hola desde el slot
+        <template slot="slot2"> Hola desde el slot II </template>
+        <template slot="slot3"> Hola desde el slot III </template>
+    </componenteSlot>
+  </div>
+</template>
+
+<script>
+import componenteSlot from "./components/componenteSlot";
+
+export default {
+  name: 'App',
+  components: {
+    componenteSlot
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/proyectogui/src/components/componenteSlot.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 style="color:red">
+            <slot> Hola, desde el componente </slot>
+        </h1>
+        <hr>
+        <h2>
+            <slot name="slot2">Hola desde el componente slot 2</slot>
+        </h2>
+        <hr>
+        <h3>
+             <slot name="slot3">Hola desde el componente slot 3</slot>
+        </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "componenteSlot",
+    }
+</script>
+
+<style scoped>
+</style>
