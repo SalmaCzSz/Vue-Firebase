df7e692c69c60da897df105ea2238716081acbdc
Salma Cruz Sánchez <63065531+SalmaCzSz@users.noreply.github.com>
2023-05-31 17:09:39 -0600

    31/05/2023 | Uso de mapState. Incluye 3 formas de acceder a las propiedades del state.

---
+++ src/vuexmapstate/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>Ejemplo de Vuex MapState</h1>
+    <p>{{this.$store.state.nombre}}</p>
+    <p>{{nombre}}</p>
+    <p>{{edad}}</p>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+
+export default {
+  name: 'App',
+  components: {
+  },
+  /*
+  computed:
+    nombre(){
+        return this.$store.state.nombre
+    }
+
+  computed: mapState ({
+    nombre: (state) => state.nombre,
+    edad: function (state) {
+        return state.edad
+    }
+  }),
+  */
+  computed: mapState(['nombre', 'edad'])
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
