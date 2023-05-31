49fb40f69b3d8f520759065b89134faabb9a74f9
Salma Cruz Sánchez <63065531+SalmaCzSz@users.noreply.github.com>
2023-05-31 16:08:09 -0600

    31/05/2023 | Propiedades en los componentes.

---
+++ src/proyectogui/src/App16.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <componentePersona nombre="Norman" edad="24"></componentePersona>
+    <hr>
+    <componentePersona nombre="Thamar" edad="22"></componentePersona>
+  </div>
+</template>
+
+<script>
+ import componentePersona from "./components/persona";
+
+export default {
+  name: 'App',
+  components: {
+    componentePersona
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/proyectogui/src/components/persona.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>El nombre es: {{nombre}}</p>
+        <p>La edad que tiene es: {{edad}}</p>
+        <componenteHijo :nombre="nombre" :edad="edad"></componenteHijo>
+    </div>
+</template>
+
+<script>
+    import componenteHijo from "./hijo";  //anidación de componentes
+
+    export default {
+        name: "componentePersona",
+        props: [        //array de propiedades
+            'nombre',
+            'edad'
+        ],
+        components: {
+            componenteHijo
+        }
+    }
+</script>
+
+<style scoped>
+</style>
