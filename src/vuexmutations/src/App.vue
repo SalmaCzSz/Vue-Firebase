ff83c90fa933212c98a0b69e167f29efa0ce43bb
Salma Cruz Sánchez <63065531+SalmaCzSz@users.noreply.github.com>
2023-06-01 17:09:14 -0600

    01/06/2023 | Se retoma el tema de mutations y se implementa nuevamente en un contador.

---
+++ src/vuexmutations/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Vuex mutations</h1>
+    <comContador></comContador>
+  </div>
+</template>
+
+<script>
+import comContador from './components/contador'
+
+export default {
+  name: 'App',
+  components: {
+    comContador
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/vuexmutations/src/components/contador.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{contador}}</p>
+    <button @click="aumentar"> + </button>
+    <button @click="reducir"> - </button>
+  </div>
+</template>
+
+<script>
+    import {mapState} from 'vuex'
+
+    export default {
+      name: 'comContador',
+      computed: mapState(['contador']),
+      methods: {
+        aumentar(){
+            //this.$store.state.contador++
+            this.$store.commit('aumentar')
+        },
+        reducir(){
+            //this.$store.state.contador--
+            this.$store.commit('reducir')
+        }
+      }
+    }
+</script>
+
+<style scoped>
+</style>
